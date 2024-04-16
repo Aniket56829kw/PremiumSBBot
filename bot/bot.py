2af3960da03960c8919db57d79cfe18279bc45bd
Aniket56829kw <132885070+Aniket56829kw@users.noreply.github.com>
2024-04-16 19:48:46 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,5 @@
 ###############################################
 
 
-Follow_me_on_Youtube = "https://www.youtube.com/c/cvatricks"
-
-Live_Bot_Link = "https://t.me/PremiumSBBot"
 
 ################################################
